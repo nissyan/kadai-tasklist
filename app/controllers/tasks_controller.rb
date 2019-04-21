be1baf344cd89264eadabcb451135694fa0d7403
nissyan <calvin.klein.yu02@gmail.com>
2019-04-21 01:57:20 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,8 +33,9 @@ class TasksController < ApplicationController
          @task = Task.find(params[:id])
     end
     
+    
     def update 
-         @task = current_user.tasks.build(task_params)
+         @task = Task.find(params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -55,9 +56,6 @@ class TasksController < ApplicationController
     
     private
     
-    def set_task
-        @task = Task.find(params[:id])
-    end
     
     def task_params
         params.require(:task).permit(:content, :status)
